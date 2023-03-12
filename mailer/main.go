775dee397c6758225a61ec5c3d0367244900dacc
ricxi <irixoc@gmail.com>
2023-03-11 22:58:00 -0500

    set up and tested email handlers and helpers

--- mailer/main.go
+++ mailer/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -13,19 +16,62 @@ func main() {
 
   m := NewMailer(conf.username, conf.password, conf.host, conf.port)
   
-  from := "team@flatlist.com"
-  to := "dwightschrute@dundermifflin.com"
-  subject := "Please activate your account"
-  body := "Hello Dwight,</br>Follow the instructions to activate your account."
+  mux := http.NewServeMux()
 
-  m.Send(from, to, subject, body)
+  mux.HandleFunc("/v1/send", handleSendEmail(m))
+
+  srv := &http.Server{
+    Handler: mux,
+    Addr: ":5000",
+  }
+
+  log.Println("starting server on port", srv.Addr)
+  if err := srv.ListenAndServe(); err != nil {
+    log.Fatal(err)
+  }
+  // from := "team@flatlist.com"
+  // to := "dwightschrute@dundermifflin.com"
+  // subject := "Please activate your account"
+  // body := "Hello Dwight,</br>Follow the instructions to activate your account."
+  // m.Send(from, to, subject, body)
 }
 
 
-func handleSendEmail(m Mailer) http.HandlerFunc {
+func handleSendEmail(m *Mailer) http.HandlerFunc {
   return func(w http.ResponseWriter, r *http.Request) {
 
+    type EmailData struct {
+      From string `json:"from"`
+      User struct {
+        FirstName string `json:"firstName"`
+        LastName string `json:"lastName"`
+        Email string `json:"email"`
+      } `json:"user"`
+    }
 
+    var data EmailData
+    if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+
+    t, err := template.ParseFiles("./useractivation.html")
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+
+    body := new(bytes.Buffer)
+    t.Execute(body, struct{FirstName string}{FirstName: data.User.FirstName})
+
+
+    subject := "email test"
+    if err := m.Send(data.From, data.User.Email, subject, body.String()); err != nil {
+      http.Error(w, err.Error(), http.StatusBadRequest)
+      return
+    } else {
+      w.Write([]byte("success!"))
+    }
   }
 }
 
