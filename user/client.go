cd77ad8eba65f135dce5d4acfa5ca0e4274abf06
ricxi <irixoc@gmail.com>
2023-03-17 01:10:22 -0400

    updated user to use updated grpc features from mailer service

--- user/client.go
+++ user/client.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/ricxi/flat-list/mailer"
 	"github.com/ricxi/flat-list/mailer/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -36,17 +37,20 @@ type grpcClient struct{}
 // SendActivationEmail makes a remote procedure call to the mailer service,
 // which sends an account activation email to a newly registered user
 func (g grpcClient) SendActivationEmail(email, name, activationToken string) error {
+	// this port should be an environment variable
 	cc, err := grpc.Dial(":5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return err
 	}
 
 	c := pb.NewMailerClient(cc)
+
+	activationHyperlink := "http://localhost/9001" + activationToken
 	if _, err := c.SendActivationEmail(context.Background(), &pb.Request{
-		From:            "the.team@flat-list.com",
-		To:              email,
-		Name:            name,
-		ActivationToken: activationToken,
+		From:                "the.team@flat-list.com",
+		To:                  email,
+		Name:                name,
+		ActivationHyperlink: activationHyperlink,
 	}); err != nil {
 		return err
 	}
@@ -56,21 +60,17 @@ func (g grpcClient) SendActivationEmail(email, name, activationToken string) err
 
 type httpClient struct{}
 
-func (h httpClient) SendActivationEmail(email, firstName, activationToken string) error {
-	activationInfo := struct {
-		From            string `json:"from"`
-		To              string `json:"to"`
-		FirstName       string `json:"firstName"`
-		ActivationToken string `json:"activationToken"`
-	}{
-		From:            "the.team@flatlist.com",
-		To:              email,
-		FirstName:       firstName,
-		ActivationToken: activationToken,
+func (h httpClient) SendActivationEmail(email, name, activationToken string) error {
+	activationHyperlink := "http://localhost/9001" + activationToken
+	data := mailer.EmailActivationData{
+		From:                "the.team@flat-list.com",
+		To:                  email,
+		Name:                name,
+		ActivationHyperlink: activationHyperlink,
 	}
 
 	reqBody := new(bytes.Buffer)
-	if err := json.NewEncoder(reqBody).Encode(&activationInfo); err != nil {
+	if err := json.NewEncoder(reqBody).Encode(&data); err != nil {
 		return err
 	}
 
--- user/go.mod
+++ user/go.mod
@@ -5,27 +5,29 @@ go 1.19
 require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/golang-jwt/jwt/v4 v4.5.0
-	github.com/ricxi/flat-list/mailer v0.0.0-20230315205350-06d5f9fd2d74
+	github.com/ricxi/flat-list/mailer v0.0.0-20230317045119-60100e35d983
 	go.mongodb.org/mongo-driver v1.11.2
 	golang.org/x/crypto v0.7.0
 	google.golang.org/grpc v1.53.0
 )
 
 require (
+	github.com/aws/aws-lambda-go v1.38.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
-	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/montanaflynn/stats v0.7.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.7.2 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
-	github.com/xdg-go/scram v1.1.1 // indirect
-	github.com/xdg-go/stringprep v1.0.3 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
-	google.golang.org/protobuf v1.29.1 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
