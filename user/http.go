5ac07cbbe639ca875d44530fc149a413cee3e05b
ricxi <irixoc@gmail.com>
2023-04-05 00:07:52 -0400

    updated all user service handlers with new shared response and request functions

--- user/http.go
+++ user/http.go
@@ -1,10 +1,6 @@
 package user
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -45,7 +41,7 @@ func NewHandler(service Service) http.Handler {
 }
 
 func (h httpHandler) handleHealthCheck(w http.ResponseWriter, r *http.Request) {
-	writeToResponse(w, Response{"status": "success", "message": "user service is running"}, http.StatusOK)
+	response.SendJSON(w, Response{"status": "success", "message": "user service is running"}, http.StatusOK, nil)
 }
 
 func (h httpHandler) handleRegister(w http.ResponseWriter, r *http.Request) {
@@ -66,96 +62,54 @@ func (h httpHandler) handleRegister(w http.ResponseWriter, r *http.Request) {
 
 func (h httpHandler) handleLogin(w http.ResponseWriter, r *http.Request) {
 	var u UserLoginInfo
-	if err := readFromRequest(w, r, &u); err != nil {
-		writeErrorToResponse(w, err.Error(), http.StatusBadRequest)
+	if err := request.ParseJSON(r, &u); err != nil {
+		sendJSONError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	uInfo, err := h.service.LoginUser(r.Context(), &u)
 	if err != nil {
-		writeErrorToResponse(w, err.Error(), http.StatusBadRequest)
+		sendJSONError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	writeToResponse(w, Response{"user": uInfo}, http.StatusOK)
+	response.SendJSON(w, Response{"user": uInfo}, http.StatusOK, nil)
 }
 
 // handleActivate is called to activate a newly registered user's account
 func (h httpHandler) handleActivate(w http.ResponseWriter, r *http.Request) {
 	activationToken := chi.URLParam(r, "token")
 	if activationToken == "" {
-		log.Println("ERROR: missing token")
-		writeErrorToResponse(w, "missing token paramater", http.StatusBadRequest)
+		sendJSONError(w, "missing activation token parameter", http.StatusBadRequest)
 		return
 	}
 
 	if err := h.service.ActivateUser(r.Context(), activationToken); err != nil {
-		writeErrorToResponse(w, err.Error(), http.StatusBadRequest)
+		sendJSONError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	writeToResponse(w, Response{"status": "success"}, http.StatusOK)
+	response.SendJSON(w, Response{"status": "success"}, http.StatusOK, nil)
 }
 
 // handleReactivate is called to generate a new activation token and resend a new activation email to a user
+// TODO: Test this method
 func (h httpHandler) handleReactivate(w http.ResponseWriter, r *http.Request) {
-
 	var u UserLoginInfo
-	if err := readFromRequest(w, r, &u); err != nil {
-		writeErrorToResponse(w, err.Error(), http.StatusBadRequest)
+	if err := request.ParseJSON(r, &u); err != nil {
+		sendJSONError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	if err := h.service.RestartActivation(r.Context(), &u); err != nil {
-		writeErrorToResponse(w, err.Error(), http.StatusBadRequest)
+		sendJSONError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	writeToResponse(w, Response{"status": "success"}, http.StatusOK)
-}
-
-func readFromRequest(w http.ResponseWriter, r *http.Request, dest any) error {
-	err := json.NewDecoder(r.Body).Decode(dest)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	response.SendJSON(w, Response{"status": "success"}, http.StatusOK, nil)
 }
 
 type Response map[string]any
-
-// implements WriterTo interface
-type errorResponseLogger struct {
-	bytes.Buffer
-}
-
-// A proxy that logs an error if one occurs while writing to the response
-func (e errorResponseLogger) WriteTo(w io.Writer) (n int64, err error) {
-	n, err = e.Buffer.WriteTo(w)
-	if err != nil {
-		log.Println("writing to response failed: ", err.Error())
-	}
-	return
-}
-
-func writeToResponse(w http.ResponseWriter, res Response, statusCode int) {
-	var bodyBuffer bytes.Buffer
-	err := json.NewEncoder(&bodyBuffer).Encode(res)
-	if err != nil {
-		// Should this panic?
-		writeErrorToResponse(w, "json encoder: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(statusCode)
-
-	b := errorResponseLogger{bodyBuffer}
-	if _, err := b.WriteTo(w); err != nil {
-		// panic and log error to avoid superfluous header warning? Best way to set the error code?
-		panic(err)
-	}
-}
 
 func sendJSONError(w http.ResponseWriter, message string, statusCode int) {
 	payload := map[string]string{
@@ -163,25 +117,4 @@ func sendJSONError(w http.ResponseWriter, message string, statusCode int) {
 	}
 
 	response.SendJSON(w, payload, statusCode, nil)
-}
-
-func writeErrorToResponse(w http.ResponseWriter, message string, statusCode int) {
-	if message == "" {
-		message = "unknown"
-	}
-
-	var bodyBuffer bytes.Buffer
-	if err := json.NewEncoder(&bodyBuffer).Encode(Response{"error": message}); err != nil {
-		// Should I panic instead?
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-
-	b := errorResponseLogger{bodyBuffer}
-	if _, err := b.WriteTo(w); err != nil {
-		panic(err)
-	}
 }
