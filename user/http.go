1d50556b37787f165a8899c2dadc7d4e47faf853
ricxi <irixoc@gmail.com>
2023-04-05 14:35:38 -0400

    user's service: shortened request and response package names to req and res, respectively

--- user/http.go
+++ user/http.go
@@ -6,8 +6,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
-	"github.com/ricxi/flat-list/shared/request"
-	"github.com/ricxi/flat-list/shared/response"
+	req "github.com/ricxi/flat-list/shared/request"
+	res "github.com/ricxi/flat-list/shared/response"
 )
 
 type httpHandler struct {
@@ -44,70 +44,70 @@ func NewHandler(service Service) http.Handler {
 type Response map[string]any
 
 func (h httpHandler) handleHealthCheck(w http.ResponseWriter, r *http.Request) {
-	response.SendJSON(w, Response{"success": true, "message": "user service is running"}, http.StatusOK, nil)
+	res.SendJSON(w, Response{"success": true, "message": "user service is running"}, http.StatusOK, nil)
 }
 
 func (h httpHandler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	var u UserRegistrationInfo
-	if err := request.ParseJSON(r, &u); err != nil {
-		response.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
+	if err := req.ParseJSON(r, &u); err != nil {
+		res.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	id, err := h.service.RegisterUser(r.Context(), &u)
 	if err != nil {
-		response.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
+		res.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	response.SendJSON(w, Response{"id": id}, http.StatusCreated, nil)
+	res.SendJSON(w, Response{"id": id}, http.StatusCreated, nil)
 }
 
 func (h httpHandler) handleLogin(w http.ResponseWriter, r *http.Request) {
 	var u UserLoginInfo
-	if err := request.ParseJSON(r, &u); err != nil {
-		response.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
+	if err := req.ParseJSON(r, &u); err != nil {
+		res.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	uInfo, err := h.service.LoginUser(r.Context(), &u)
 	if err != nil {
-		response.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
+		res.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	response.SendJSON(w, Response{"user": uInfo}, http.StatusOK, nil)
+	res.SendJSON(w, Response{"user": uInfo}, http.StatusOK, nil)
 }
 
 // handleActivate is called to activate a newly registered user's account
 func (h httpHandler) handleActivate(w http.ResponseWriter, r *http.Request) {
 	activationToken := chi.URLParam(r, "token")
 	if activationToken == "" {
-		response.SendErrorJSON(w, "missing activation token parameter", http.StatusBadRequest)
+		res.SendErrorJSON(w, "missing activation token parameter", http.StatusBadRequest)
 		return
 	}
 
 	if err := h.service.ActivateUser(r.Context(), activationToken); err != nil {
-		response.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
+		res.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	response.SendJSON(w, Response{"status": "success"}, http.StatusOK, nil)
+	res.SendJSON(w, Response{"status": "success"}, http.StatusOK, nil)
 }
 
 // handleReactivate is called to generate a new activation token and resend a new activation email to a user
 // TODO: Test this method
 func (h httpHandler) handleReactivate(w http.ResponseWriter, r *http.Request) {
 	var u UserLoginInfo
-	if err := request.ParseJSON(r, &u); err != nil {
-		response.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
+	if err := req.ParseJSON(r, &u); err != nil {
+		res.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	if err := h.service.RestartActivation(r.Context(), &u); err != nil {
-		response.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
+		res.SendErrorJSON(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	response.SendJSON(w, Response{"status": "success"}, http.StatusOK, nil)
+	res.SendJSON(w, Response{"status": "success"}, http.StatusOK, nil)
 }
