ccb1c0d282fe995701adc606e4e6d3f27f3195f3
ricxi <irixoc@gmail.com>
2023-04-21 12:57:39 -0400

    created middleware to process jwt token for task service

---
+++ task/context.go
@@ -0,0 +1,19 @@
+package task
+
+import (
+	"context"
+	"errors"
+)
+
+type ContextKey string
+
+var UserIDCtxKey ContextKey = ContextKey("userId")
+
+func getUserIDFromCtx(ctx context.Context) (string, error) {
+	userID, ok := ctx.Value(UserIDCtxKey).(string)
+	if !ok {
+		return "", errors.New("userId not found in context")
+	}
+
+	return userID, nil
+}
